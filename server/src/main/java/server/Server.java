9de5edb759fa866f6d0b158f5123f1f2e9455167
nashd <nashdav000@gmail.com>
2026-07-13 10:53:46 -0600

    Created Data Model classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
 
 public class Server {
 
@@ -10,15 +11,25 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-
+        javalin.delete("/name/{name}", this::clear);
+        javalin.post("/username/{username}/password/{password}/email/{email}", this::register);
     }
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
+
         return javalin.port();
     }
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void clear(Context ctx){
+
+    }
+
+    private void register(Context ctx){
+        System.out.println("Register request received!");
     }
 }
