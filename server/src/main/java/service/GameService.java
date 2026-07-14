80e7f228d5fb36342cf5ba2b70c1293f7094e4e3
nashd <nashdav000@gmail.com>
2026-07-13 18:20:37 -0600

    Created classes for GameService and GameDAO

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,22 @@
 package service;
 
+import service.GameClasses.*;
+
 public class GameService {
+
+    public CreateResult createGame(CreateRequest request){
+
+    }
+
+    public GetResult getGame(GetRequest request){
+
+    }
+
+    public ListResult listGames(ListRequest request){
+
+    }
+
+    public UpdateResult updateGame(UpdateRequest request){
+
+    }
 }
