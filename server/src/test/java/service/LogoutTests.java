ddede5064fa3bc130f682c693436d85220e4702f
nashd <nashdav000@gmail.com>
2026-07-15 11:37:51 -0600

    Created logout tests

--- server/src/test/java/service/LogoutTests.java
+++ server/src/test/java/service/LogoutTests.java
@@ -1,17 +1,44 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
 import org.junit.jupiter.api.*;
+import service.UserClasses.LogoutRequest;
+import service.UserClasses.RegisterRequest;
 
 public class LogoutTests {
+    private final static UserService service = new UserService(new MemoryUserDAO(), new MemoryAuthDAO());
+    private String authToken;
+
+    @BeforeEach
+    public void init() throws DataAccessException {
+        service.clearUsers();
+        service.clearAuths();
+
+        String username = "Broski";
+        String password = "password";
+        String email = "test@email.com";
+
+        RegisterRequest request = new RegisterRequest(username, password, email);
+        authToken = service.register(request).authToken();
+    }
+
     @Test
     @DisplayName("Logout: Success")
     public void logoutExistingUser(){
-
+        Assertions.assertDoesNotThrow(()->{
+            LogoutRequest request = new LogoutRequest(authToken);
+            service.logout(request);
+        });
     }
 
     @Test
     @DisplayName("Logout: Unauthorized")
     public void logoutUnauthorized(){
-
+        Assertions.assertThrows(DataAccessException.class, ()->{
+            LogoutRequest request = new LogoutRequest("badAuthToken");
+            service.logout(request);
+        });
     }
 }
