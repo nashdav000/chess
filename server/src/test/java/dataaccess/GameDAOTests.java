0c38a9c0c85fb31f3c5cd18e39801055198121e6
nashd <nashdav000@gmail.com>
2026-07-21 17:17:39 -0600

    Wrote rest of the gameDAO tests

--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -1,6 +1,10 @@
 package dataaccess;
 
+import model.GameData;
 import org.junit.jupiter.api.*;
+
+import java.util.Collection;
+import java.util.List;
 
 public class GameDAOTests {
 
@@ -49,26 +53,57 @@ public class GameDAOTests {
 
     @Test
     @DisplayName("List Game: Success")
-    public void listGameSuccess(){
+    public void listGameSuccess() throws Exception {
+        gameDAO.createGame("game1");
+        gameDAO.createGame("game2");
 
+        List<GameData> games = (List<GameData>) gameDAO.listGames();
+        Assertions.assertNotNull(games);
+
+        Assertions.assertEquals("game1", games.get(0).gameName());
+        Assertions.assertEquals("game2", games.get(1).gameName());
     }
 
     @Test
     @DisplayName("List Game: Fail")
-    public void listGameFail(){
+    public void listGameFail() throws Exception {
+        Assertions.assertDoesNotThrow(() -> {
+            gameDAO.listGames();
+        });
 
+        gameDAO.createGame("game");
+        gameDAO.clearGames();
+
+        Collection<GameData> games = gameDAO.listGames();
+        Assertions.assertNotNull(games);
     }
 
     @Test
     @DisplayName("Set Game: Success")
-    public void setGameSuccess(){
+    public void setGameSuccess() throws Exception {
+        String id = gameDAO.createGame("game");
+        GameData game = gameDAO.getGame(id);
+        GameData updatedGame = new GameData(game.gameID(),
+                "UserJoined!", game.blackUsername(), game.gameName(), game.chessGame());
+
+        Assertions.assertNotEquals(game, updatedGame);
+        Assertions.assertEquals(game.gameID(), updatedGame.gameID());
+
+        gameDAO.setGame(id, updatedGame);
+        GameData retrievedGame = gameDAO.getGame(id);
+
+        Assertions.assertEquals(updatedGame, retrievedGame);
+        Assertions.assertEquals(updatedGame.gameID(), retrievedGame.gameID());
 
     }
 
     @Test
     @DisplayName("Set Game: Fail")
-    public void setGameFail(){
-
+    public void setGameFail() throws Exception {
+        String gameID = gameDAO.createGame("game");
+        Assertions.assertThrows(Exception.class, () -> {
+            gameDAO.setGame(null, null);
+        });
     }
 
     @Test
